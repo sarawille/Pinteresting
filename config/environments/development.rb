c8cb5822694d3aab846591c41f794dd1ab28880f
Sara Wille <wille.sk@gmail.com>
2015-09-09 18:55:21 +0900

    Manual setup for devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Required for Heroku.  Sets the defaul link in "forgot password" emails.
+  config.action_mailer.default_url_options = { host: 'localhost', port:3000 }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Required for Heroku.  Sets the defaul link in "forgot password" emails.
+    # Must be set to actual URL name.
+  config.action_mailer.default_url_options = { host: 'omr-sara-pinteresting.herokuapp.com', port:3000 }
 end
