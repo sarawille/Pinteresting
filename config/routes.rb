762af119b95638d76a5c832eb7bb2f2c22ae5496
Sara Wille <wille.sk@gmail.com>
2015-09-16 15:59:09 +0900

    Change devise layout/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'pages#home' # creates root_path
   get "about" => 'pages#about' # creates about_path
 
